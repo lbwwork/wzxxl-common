60b9f88847cf4462972e56281a3695c266fcdced
libaowen <1139780848@qq.com>
2023-11-07 10:45:28 +0800

    项目搭建

---
+++ src/main/java/com/wzxxl/common/WzxxlCommonApplication.java
@@ -0,0 +1,13 @@
+package com.wzxxl.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzxxlCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WzxxlCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzxxl/common/WzxxlCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzxxl.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzxxlCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
